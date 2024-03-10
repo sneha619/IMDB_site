007a6c5391f367c119971ba2585d67b4faed9368
sneha619 <sneha1995sarkar25@gmail.com>
2024-03-11 01:44:46 +0530

    First commit

--- src/Components/MovieFavourites.js
+++ src/Components/MovieFavourites.js
@@ -1,9 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const MovieFavourites = () => {
+    const [favourites, setFavourites] = useState([]);
 
     useEffect (() => {
-        const favouritesData = localStorage.getItem("favourites");
+        const favouritesData = localStorage.getItem("favourites") || "[]";
+        setFavourites(JSON.parse(favouritesData));
     },[]);
 
     return(
@@ -23,6 +25,20 @@ const MovieFavourites = () => {
                                 <th>Actions</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            {
+                                favourites.map((favourite) => {
+                                    <tr>
+                                        <td><img src={`https://image.tmdb.org/t/p/w500${favourite.poster_path}`} /></td>
+                                        <td>{favourite.title}</td>
+                                        <td>{favourite.genre_ids[0]}</td>
+                                        <td>{favourite.popularity}</td>
+                                        <td>{favourite.vote_average}</td>
+                                        <td><button>Delete</button></td>
+                                    </tr>
+                                })
+                            }
+                        </tbody>
                     </table>
                 </div>
             </div>
